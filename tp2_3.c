a30d4a955876e0aec4322193729df830dddd4f74
Gaston <gastonvalla1@gmail.com>
2025-04-27 12:30:37 -0300

    tp2_3

---
+++ tp2_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 5
+#define M 7
+
+int main() 
+{
+    srand(time(NULL));
+    int i, j;
+    int mt[N][M];
+    int *p;
+
+    p = mt;
+
+    for(i = 0; i < N; i++) {
+        for(j = 0; j < M; j++) {
+            *(p+i*M+j) = 1+rand()%100;
+            printf("%d   ", *(p + i*M + j));
+        }
+        printf("\n");
+    }
+    return 0;
+}
