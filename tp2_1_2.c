b651ddbac4e20bf9f0a921e2ef6456c46881a546
Gaston <gastonvalla1@gmail.com>
2025-04-26 22:41:22 -0300

    opcion 2

---
+++ tp2_1_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 20 
+
+int main()
+{
+    srand(time(NULL));
+    int i; 
+    double vt[N];
+    double *p;
+    p = vt;
+
+    for(i = 0;i<N; i++) 
+    {  
+        *(p+i)=1+rand()%100; 
+        printf("%f   ", *(p+i));
+    }
+    
+    return 0;
+}
